b5995a54e50076e3ececc04ee23a38be6a65adf2
zhougy <zhougy1996@outlook.com>
2025-07-03 20:50:56 +0800

    [build] [DogeDynamicLibrary] Add project template.

---
+++ Engine/Source/Template/DogeDynamicLibrary/Private/PCH.DogeDynamicLibrary.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define MODULE_NAME "DogeDynamicLibrary"
+
+#include "CoreMinimal.h"
+
+#include "Definitions.DogeDynamicLibrary.h"
---
+++ Engine/Source/Template/DogeDynamicLibrary/Public/Definitions.DogeDynamicLibrary.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "HAL/Platform.h"
+
+#include "Definitions.Core.h"
+
+//#ifdef PROJECTNAME_EXPORTS
+//	#define PROJECTNAME_API DLLEXPORT
+//#else
+//	#define PROJECTNAME_API DLLIMPORT
+//#endif
