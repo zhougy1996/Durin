099275c773ff2d1195971a45c3eb674e182e2677
zhougy <zhougy1996@outlook.com>
2025-08-25 11:11:36 +0800

    [Build] [DogeHeaderTool] Extract frequently-used cmake functions for DHT.

--- Engine/Configs/CMake/DogeModules.cmake
+++ Engine/Configs/CMake/DogeModules.cmake
@@ -1,62 +1,99 @@
+# DHT (Doge Header Tool) Integration
+set(DHT_EXE "${DOGE_SCRIPT_DIR}/TestParse.py")
+
+function (doge_module_get_dht_output_directory module_name out_directory)
+	set(_dht_output_directory "${DOGE_INTERMEDIATE_DIR}/${module_name}/${DOGE_ARCH}/DHT")
+	set(${out_directory} ${_dht_output_directory} PARENT_SCOPE)
+endfunction()
+
+function (doge_set_dht_input_headers input_headers output_directory out_generated_files)
+	foreach(header ${input_headers})
+		message("DHT Input Header: ${header}")
+		get_filename_component(header_name ${header} NAME_WE)
+		set(_gen_cpp_file ${output_directory}/${header_name}.gen.cpp)
+		set(_gen_h_file ${output_directory}/${header_name}.gen.h)
+
+		list(APPEND _generated_files ${_gen_cpp_file})
+		list(APPEND _generated_files ${_gen_h_file})
+
+		add_custom_command(
+			OUTPUT ${_generated_files}
+			COMMAND python ${DHT_EXE} ${header} ${output_directory}
+			DEPENDS ${header}
+			COMMENT "DHT Parsing ${header_name}.h"
+		)
+	endforeach()
+	set_source_files_properties(${_generated_files} PROPERTIES GENERATED TRUE)
+	set(${out_generated_files} ${_generated_files} PARENT_SCOPE)
+endfunction()
+
+function (doge_module_add_dht_input_headers module_name input_headers out_generated_files)
+	doge_module_get_dht_output_directory(${module_name} _dht_output_directory)
+	doge_set_dht_input_headers(${input_headers} ${_dht_output_directory} _generated_files)
+
+	set(${out_generated_files} ${_generated_files} PARENT_SCOPE)	
+endfunction()
+
+# Module Setup Functions
 function(doge_set_module_output target)
-    set_target_properties(${target} PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/Doge/${DOGE_ARCH}/$<CONFIG>"
-        LIBRARY_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
-        ARCHIVE_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
-        PDB_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
-    )
+	set_target_properties(${target} PROPERTIES
+		RUNTIME_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/Doge/${DOGE_ARCH}/$<CONFIG>"
+		LIBRARY_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
+		ARCHIVE_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
+		PDB_OUTPUT_DIRECTORY "${DOGE_BINARY_DIR}/${target}/${DOGE_ARCH}/$<CONFIG>"
+	)
 endfunction()
 
 function(doge_print_project_build_info)
-    message("-- Build ${PROJECT_NAME}")
+	message("-- Build ${PROJECT_NAME}")
 endfunction()
 
 function(doge_organize_source_files)
-    foreach(source ${ARGV})
-        file(RELATIVE_PATH relative_path ${CMAKE_CURRENT_SOURCE_DIR} ${source})
-        get_filename_component(group_dir ${relative_path} DIRECTORY)
+	foreach(source ${ARGV})
+		file(RELATIVE_PATH relative_path ${CMAKE_CURRENT_SOURCE_DIR} ${source})
+		get_filename_component(group_dir ${relative_path} DIRECTORY)
 
-        if(NOT group_dir)
-            set(group_name "Source Files")
-        else()
-            string(REPLACE "/" "\\" group_name ${group_dir})
-        endif()
+		if(NOT group_dir)
+			set(group_name "Source Files")
+		else()
+			string(REPLACE "/" "\\" group_name ${group_dir})
+		endif()
 
-        source_group("${group_name}" FILES ${source})
-    endforeach()
+		source_group("${group_name}" FILES ${source})
+	endforeach()
 endfunction()
 
 function(doge_collect_and_organize_source_files OUT_SRCS)
-    set(patterns ${ARGN})
-        if(NOT patterns)
-            set(patterns 
-                "*.h" "*.hpp" "*.hxx"
-                "*.c" "*.cpp" "*.cxx" "*.cc"
-            )
-    endif()
-    file(GLOB_RECURSE all_sources CONFIGURE_DEPENDS ${patterns})
-    doge_organize_source_files(${all_sources})
-    set(${OUT_SRCS} ${all_sources} PARENT_SCOPE)
+	set(patterns ${ARGN})
+		if(NOT patterns)
+			set(patterns 
+				"*.h" "*.hpp" "*.hxx"
+				"*.c" "*.cpp" "*.cxx" "*.cc"
+			)
+	endif()
+	file(GLOB_RECURSE all_sources CONFIGURE_DEPENDS ${patterns})
+	doge_organize_source_files(${all_sources})
+	set(${OUT_SRCS} ${all_sources} PARENT_SCOPE)
 endfunction()
 
-function(doge_setup_shared_library target)
-    doge_set_module_output(${target})
-    set_target_properties(${target} PROPERTIES OUTPUT_NAME "DogeEditor-${target}")
-    string(TOUPPER "${target}" uppercase_target)
+function(doge_setup_shared_library module_name)
+	doge_set_module_output(${module_name})
+	doge_module_get_dht_output_directory(${module_name} _dht_output_directory)
+	set_target_properties(${module_name} PROPERTIES OUTPUT_NAME "DogeEditor-${module_name}")
+	string(TOUPPER "${module_name}" uppercase_module_name)
 
-    # target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-W4>)
+	target_compile_definitions(${module_name} PRIVATE ${uppercase_module_name}_EXPORTS)
+	target_compile_definitions(${module_name} PRIVATE MODULE_NAME="${module_name}")
 
-    target_compile_definitions(${target} PRIVATE ${uppercase_target}_EXPORTS)
-    target_compile_definitions(${target} PRIVATE MODULE_NAME="${target}")
-
-    target_include_directories(${target} PRIVATE
-        ${CMAKE_CURRENT_SOURCE_DIR}/Private
-    )
-    target_include_directories(${target} PUBLIC
-        ${CMAKE_CURRENT_SOURCE_DIR}/Public
-    )
-    target_precompile_headers(${target} PRIVATE
-        "$<$<COMPILE_LANGUAGE:CXX>:${CMAKE_CURRENT_SOURCE_DIR}/Private/PCH.${target}.h>"
-    )
-    install(FILES $<TARGET_FILE:${target}> DESTINATION bin)
+	target_include_directories(${module_name} PRIVATE
+		${CMAKE_CURRENT_SOURCE_DIR}/Private
+	)
+	target_include_directories(${module_name} PUBLIC
+		${CMAKE_CURRENT_SOURCE_DIR}/Public
+		${_dht_output_directory}
+	)
+	target_precompile_headers(${module_name} PRIVATE
+		"$<$<COMPILE_LANGUAGE:CXX>:${CMAKE_CURRENT_SOURCE_DIR}/Private/PCH.${module_name}.h>"
+	)
+	install(FILES $<TARGET_FILE:${module_name}> DESTINATION bin)
 endfunction()
---
+++ Engine/Source/Editor/LevelEditor/Private/TestDHT.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "TestDHT.gen.h"
