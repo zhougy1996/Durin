209a39d6e8bf59e6837985da169a1212e9329182
zhougy <zhougy1996@outlook.com>
2026-05-22 11:14:12 +0800

    [Refactor] [RenderCore] Change shader code representation from uint32 to std::byte.

--- Engine/Source/Runtime/VulkanRHI/Private/VulkanShader.cpp
+++ Engine/Source/Runtime/VulkanRHI/Private/VulkanShader.cpp
@@ -11,8 +11,13 @@ namespace Durin::VulkanRHI
 		, EntryPoint(InCreateDesc.EntryPoint)
 	{
 		vk::ShaderModuleCreateInfo createInfo;
-		createInfo.setCode(InCreateDesc.Code);
-
+		// The shader code is expected to be in SPIR-V bytecode format, which is a binary format where each instruction is 4 bytes (32 bits) long. Therefore, the size of the code should be a multiple of 4 bytes.
+		check(InCreateDesc.Code.size() % sizeof(uint32) == 0);
+		std::span Code = {
+			reinterpret_cast<const uint32*>(InCreateDesc.Code.data()),
+			InCreateDesc.Code.size() / sizeof(uint32)
+		};
+		createInfo.setCode(Code);
 		ShaderModule = Device.GetHandle().createShaderModule(createInfo);
 	}
 
