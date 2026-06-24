a3f5bb8f1d7cfeffb2a35ed24ebe3e0dfaa75a03
zhougy <zhougy1996@outlook.com>
2026-06-24 21:18:32 +0800

    feat(DObject): Add TObjectPtr class for enhanced object pointer management

---
+++ Engine/Source/Runtime/CoreDObject/Public/DObject/ObjectPtr.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "DObject/Object.h"
+
+namespace Durin
+{
+	class FObjectPtrBase
+	{
+	public:
+		auto GetObject() const -> DObject* { return Object; }
+
+	protected:
+		DObject* Object = nullptr;
+	};
+
+	template<typename T>
+	class TObjectPtr : public FObjectPtrBase
+	{
+	public:
+		TObjectPtr() = default;
+		TObjectPtr(std::nullptr_t) {}
+		TObjectPtr(T* InObject) { Object = ToDObject(InObject); }
+
+		auto Get() const -> T* { return static_cast<T*>(Object); }
+		auto Reset() -> void { Object = nullptr; }
+
+		auto operator=(std::nullptr_t) -> TObjectPtr&
+		{
+			Object = nullptr;
+			return *this;
+		}
+
+		auto operator=(T* InObject) -> TObjectPtr&
+		{
+			Object = ToDObject(InObject);
+			return *this;
+		}
+
+		auto operator->() const -> T* { return Get(); }
+		operator T*() const { return Get(); }
+		explicit operator bool() const { return Object != nullptr; }
+
+	private:
+		static auto ToDObject(T* InObject) -> DObject*
+		{
+			return InObject;
+		}
+	};
+}
